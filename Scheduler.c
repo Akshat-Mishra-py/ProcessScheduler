dfa9cc58169af9ef59a43da42210069f5739c3e5
Akshat Mishra <akshat.mishra2316@gmail.com>
2026-07-23 23:17:50 +0530

    Complete NonPrimitiveScheduler() {i didnt compile yet}

--- Scheduler.c
+++ Scheduler.c
@@ -1,11 +1,35 @@
 #include "ProcessManager.h"
+#include "utilities.h"
 #include "scheduler.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <Windows.h>
 
 // This file contains scheduler implementations.
 // The current function is left empty and can be completed later.
 
-void NonPrimitiveScheduler(struct Processes* p, int size){
-    
+void NonPrimitiveScheduler(struct Processes* p, int size, enum Criteria c){
+    int time = 0;
+    int index = nextProcess(p, size, time, c);
+    while (size>=0)
+    {
+        if (index<=0){
+            printf("CPU: Idle");
+            time=Wait(time);
+            index = nextProcess(p, size, time, c);
+        }
+        if ( p[index].actualBurstTime<=0 ){
+            size = removeProcess(p, index, size);
+            index = nextProcess(p, size, time, c);
+        }
+        else{
+            printf("CPU: Computing Process: #", p[index].pid);
+            p[index].actualBurstTime--;
+            time = Wait(time);
+        }
+    }    
+}
+int Wait(int currentTime){
+    currentTime++;
+    Sleep(1000);
 }
